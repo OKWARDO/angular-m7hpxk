9b21d72011a2e991b98dcba49e51f85d79dee66b
OKWARDO <jamesk33ward@gmail.com>
2020-05-29 16:36:23 -0700

    4 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
   providers: [],
   bootstrap:    [ AppComponent ]
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,16 +21,14 @@ export class HeroesComponent implements OnInit {
     
 
 
-  constructor(private heroService: HeroService,private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
      this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  
 
- }
+ 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
