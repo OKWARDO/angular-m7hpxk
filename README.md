d8982d38415d8cf18cb1f7c8afeb71e4170d4e0a
OKWARDO <jamesk33ward@gmail.com>
2020-05-28 22:28:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-m7hpxk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-m7hpxk)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeroesComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
